63d384be37656e89db37dc805755bf41ddf6541a
kaakchan <moeook18@gmail.com>
2025-08-16 17:23:13 +0630

    loopwhilerange

---
+++ coreyschaferYT/l5loop.py
@@ -0,0 +1,38 @@
+# nums = [1,2,3,4,5]
+
+# # for num in nums:
+# #     print(num)
+
+# # for num in nums:
+# #     if num == 3:
+# #         print("Found")
+# #         break                  # end here 1 2 found
+# #     print(num)
+
+
+# # for num in nums:
+# #     if num == 3:
+# #         print("Found")
+# #         continue               #  1 2 found 4 5
+# #     print(num)
+
+# # for num in nums:
+# #     for letter in 'abc':
+# #         print(num, letter) # 1 a 1b 1c ...........
+
+# # for i in range(10):
+# #     print(i)  # 0 to 9
+
+# # for i in range(1,11):
+# #     print(i)  # 1 to 10
+
+x = 0 
+# while x < 10:
+#     print(x)
+#     x += 1
+
+# while x < 10:
+#     if x == 5:
+#         break
+#     print(x)
+#     x += 1
